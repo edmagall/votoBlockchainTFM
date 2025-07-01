ceb21ef3e34818aac0d9b373c9f4b210f6baf7d9
edmagall <91624101+edmagall@users.noreply.github.com>
2025-07-01 18:42:02 +0200

    Archivos carpeta raíz

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      // red local simulada, sin configuración extra necesaria
+    },
+  },
+};
+require("@nomicfoundation/hardhat-toolbox");
+
